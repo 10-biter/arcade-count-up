8c7d05b39ff6c64c07de62e6b0afe07ae3b7f8d7
10-biter <ep.student@hotmail.com>
2025-12-07 16:58:38 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+namespace info {
+    //%block
+    export function countUp(): void {
+        info.startCountdown(1)
+        forever(function() {
+            info.changeCountdownBy(1)
+        })
+    }
+}
